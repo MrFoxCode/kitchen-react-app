1c8bf7ac32758f6d3b0473e7319141dfcd5aa1f9
Mr. Fox <fox.contas@hotmail.com>
2022-06-22 17:38:57 -0300

    added more features to recipes

--- kitchen-react/src/components/recipes.js
+++ kitchen-react/src/components/recipes.js
@@ -1,5 +1,9 @@
 import React from "react";
 import RecipesAll from "./recipeseach";
+import pancakeImg from "../images/pancake.jpg";
+import cakeImg from "../images/cake.jpg";
+import drinkImg from "../images/drink.jpg";
+import burgerImg from "../images/burger.jpg";
 
 function RecipesMain(){
 
@@ -8,15 +12,14 @@ function RecipesMain(){
     <div className="all-containers">
         
         <div className="recipes">
-           <a href=""> <RecipesAll name="Name 1" about="about recipe1" /> </a>
-           <a href=""> <RecipesAll name="Name 2" about="about recipe2" /> </a>
-           <a href=""> <RecipesAll name="Name 3" about="about recipe3" /> </a>
-           <a href=""> <RecipesAll name="Name 4" about="about recipe4" /> </a>
-            
+           <a href=""> <RecipesAll image = {pancakeImg} name="Category 1" about="about recipe1" /> </a>
+           <a href=""> <RecipesAll image = {cakeImg} name="Category 2" about="about recipe2" /> </a>
+           <a href=""> <RecipesAll image = {burgerImg} name="Category 3" about="about recipe3" /> </a>
+           <a href=""> <RecipesAll image = {drinkImg} name="Category 4" about="about recipe4" /> </a>            
         </div>
 
         <div className="see-all-recipes">
-            <a href = "">See all Recipes</a>
+            <a href = "" alt="see all recipes">See all Recipes</a>
         </div>
 
     </div>
--- kitchen-react/src/components/recipeseach.js
+++ kitchen-react/src/components/recipeseach.js
@@ -1,12 +1,35 @@
-import React from "react";
+import React, {useState} from "react";
 
 //each recipe inside the div recipes in recipes.js
 function RecipesAll(props){
-    return(    
-    <div className="recipe">
+
+    const [inOver, setInOver] = useState(false);
+
+
+    const recipeBG = {
+        backgroundPosition: 'bottom',
+        backgroundSize: 'cover',
+        backgroundImage: `linear-gradient(rgba(0,0,0,0.4), rgba(0,0,0,0.5)), url(${props.image})`
+    };
+
+    function changeBG(e){
+       // e.target.style.backgroundSize = '280px';   
+        e.target.style.backgroundImage = `linear-gradient(rgba(0,0,0,0.0),rgba(0,0,0,0.1)), url(${props.image})`
+    };
+
+    function changeBGOut(e){        
+        e.target.style.backgroundPosition = 'bottom';     
+        e.target.style.backgroundSize = 'cover';   
+        e.target.style.backgroundImage = `linear-gradient(rgba(0,0,0,0.4),rgba(0,0,0,0.5)), url(${props.image})`
+       };
+   
+    return(
+    //the Styles import a image, for each recipe at the recipes section    
+    //<div className="recipe" style={{ backgroundImage: `url(${props.image})` }}>       
+    <div className="recipe" style={recipeBG} onMouseEnter={changeBG} onMouseLeave={changeBGOut}>       
     <h1>{props.name}</h1>
     <p>{props.about}</p>
-    <button>See Recipe</button> 
+    <button alt='See Recipe'>See Recipe</button> 
     </div>
     );
 }
